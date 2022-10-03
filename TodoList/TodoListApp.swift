8d33228ad30a800eb1495a20dc05edf9cd8e0a6b
omar <omarabomattr@gmail.com>
2022-10-03 17:00:33 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by OmarMattr on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
